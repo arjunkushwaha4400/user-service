f016ee4d9b43fd626a28dc557b0e4602c002fc94
Arjun Kushwaha <arjun.kushwaha20234037@gmail.com>
2026-01-20 18:57:31 +0000

    add const

--- src/main/java/com/example/user_service/request.java
+++ src/main/java/com/example/user_service/request.java
@@ -1,8 +1,12 @@
 package com.example.user_service;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class request {
     private String name;
     private String email;
